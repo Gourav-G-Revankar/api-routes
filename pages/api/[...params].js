1252329ac5a851664ff5372402277c7716f99970
Gourav-G-Revankar <gourav.revankar35@gmail.com>
2022-02-23 08:46:10 +0530

    api-routes-examples

---
+++ pages/api/[...params].js
@@ -0,0 +1,7 @@
+// 5) Catch All
+
+export default function handler(req, res) {
+  const params = req.query.params
+  console.log(params)
+  res.status(200).json(params)
+}
---
+++ pages/api/blog/index.js
@@ -0,0 +1,5 @@
+// 1) File based routes
+
+export default function handler(req, res) {
+  res.status(200).json({ name: 'Blog API route' })
+}
